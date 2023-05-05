0007b54615cda88d406e9bc7f592725d583fa014
jesseleach <jesserobertleach@gmail.com>
2023-05-04 21:04:26 -0400

    completed project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 import { List } from "./Components/List";
 
 function App() {
+  const [filterValue, setFilterValue] = useState();
+
+  const handleChange = (event) => {
+    setFilterValue(event.target.value);
+  };
+
   return (
     <div className="App">
       <div className="Header">
-        <h1>Restaurants and Bars of Bushwick</h1>
-        <input className="searchBar" type="text" placeholder="Search" />
+        <h1>Bushwick Brooklyn Reddit Search</h1>
+        <input
+          onChange={handleChange}
+          className="searchBar"
+          type="text"
+          placeholder="Search"
+        />
       </div>
-      <List />
+      <List filterValue={filterValue.toLowerCase()}/>
     </div>
   );
 }
